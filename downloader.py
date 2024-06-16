f5db700580fcce69f90c711c47d7ac2013558a05
Bhavesh Padharia <padharia.bhavesh06@gmail.com>
2024-06-16 23:49:59 +0530

    update download files

--- downloader.py
+++ downloader.py
@@ -1,4 +1,4 @@
-from pytube import YouTube
+import os
 
 # Function to download YouTube video
 def download_youtube_video(url, output_path):
@@ -12,6 +12,9 @@ def download_youtube_video(url, output_path):
         # Download the video
         stream.download(output_path=output_path)
 
-        return f"Video downloaded successfully and saved to {output_path}"
+        # Get the filename of the downloaded video
+        filename = os.path.basename(stream.default_filename)
+
+        return f"Video downloaded successfully. You can find it in your device's download folder as '{filename}'."
     except Exception as e:
         return f"An error occurred: {e}"
